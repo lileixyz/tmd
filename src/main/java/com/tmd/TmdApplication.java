1caf533f987a3c47f7ae32a44803811ede55615e
lileixyz <lileixyz@sina.com>
2025-09-29 16:21:16 +0800

    Create TmdApplication.java

---
+++ src/main/java/com/tmd/TmdApplication.java
@@ -0,0 +1,13 @@
+package com.tmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmdApplication.class, args);
+    }
+
+}
